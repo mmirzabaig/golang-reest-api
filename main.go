e53f9d8f80ba6679218212616a8bc72b1e6fbaed
Mirza Baig <mirzabaiglatx@gmail.com>
2019-10-12 20:05:17 -0500

    setting up router and get request

--- main.go
+++ main.go
@@ -1,8 +1,36 @@
-
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// book struct (Model)
+type Book struct {
+	ID     string  `json: "id"`
+	Isbn   string  `json: "isbn"`
+	Title  string  `json: "title"`
+	Author *Author `json: "id"`
+}
+
+// author struct
+type Author struct {
+	Firstname string `json: "firstname`
+	Lastname  string `json: "lastname`
+}
+
+// get all books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+
+}
 
 func main() {
 	fmt.Println("Hello World!")
+	// init Router
+	router := mux.NewRouter()
+
+	// route handlers
+	router.HandleFunc("/api/books", getBooks()).Methods("GET")
 }
